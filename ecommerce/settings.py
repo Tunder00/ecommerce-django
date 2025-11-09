ad2de74ec6280d7e99dffecca3148380f60e9410
kalyan b n <kalyanbn19@gmail.com>
2025-11-09 17:58:14 +0530

    Update ALLOWED_HOSTS for production settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -29,7 +29,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://ecommerce-django-7j11.onrender.com','localhost','127.0.0.1']
 
 
 # Application definition
